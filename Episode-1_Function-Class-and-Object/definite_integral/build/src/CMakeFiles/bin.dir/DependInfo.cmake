062c300c455bc214b645d1e57ea84fc0839dbd6a
lolimay <lolimay@lolimay.cn>
2019-04-10 22:05:23 +0800

    remove .gitignore from sub-projects

---
+++ Episode-1_Function-Class-and-Object/definite_integral/build/src/CMakeFiles/bin.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/lolimay/Code/C++/cpp-practice/Episode-1_Function-Class-and-Object/definite_integral/src/definite_integral.cpp" "/media/lolimay/Code/C++/cpp-practice/Episode-1_Function-Class-and-Object/definite_integral/build/src/CMakeFiles/bin.dir/definite_integral.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
